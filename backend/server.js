069021a3d9b634eaada625e71bee89998be9e5d5
techmaxus <lakshay.mc19@nsut.ac.in>
2022-03-31 20:35:15 +0530

    database Connected

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,32 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors= require('cors');
+const mongoose = require('mongoose');
+const blogRoutes=require('./routes/blog')
+
 require('dotenv').config();
 //app
 
 const app = express();
+
+//db
+mongoose
+    .connect(process.env.DATABASE)
+    .then(() => console.log('DB connected'));
 //middleware use by app
 // morgan specify what enivironment it is now it is at development environment
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 //cors 
+if(process.env.NODE_ENV = 'development'){
+    app.use(cors({origi: `${process.env.CLIENT_URL}`}));
+}
 app.use(cors());
-//routes 
-app.get('/api',(req, res) => {
-    res.json({time:Date().toString()});
- })
+
+//routes middleware
+app.use('/api',blogRoutes);
+
 
 //accessing port value from env file or run the or value
  const port =process.env.PORT || 8000;
